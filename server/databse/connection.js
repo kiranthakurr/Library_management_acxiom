de2572b6d76679a9a37876ba8f7305ed9569b0d9
Kiranjeet <heykiranthakur@gmail.com>
2024-05-02 01:21:29 +0530

    commit 2

--- server/databse/connection.js
+++ server/databse/connection.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connection = async ()=> {
 
-    const url = process.env.DATABASE_URL;
+    const url = "mongodb+srv://heykiranthakur:wbVPJy6ipV0ZwmSc@clusterlibrarymanagemen.bmuily3.mongodb.net/?retryWrites=true&w=majority&appName=ClusterlibraryManagement";
     try{
       await mongoose.connect(url);
       console.log("database connection established")
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(cors()); 
 app.use('/', route);
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT,()=>{
     console.log(`server listening on ${PORT}`);
 })
